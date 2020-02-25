f1335b9ef54d86fb939aca79402eef6c43e3b5c9
Nimish Vithalani <nimish.v@somaiya.edu>
2020-02-25 20:20:06 +0530

    Destructuring done

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2]
 
-
-
-
-geocode('Andheri',(error,data)=>{
-    if(error){
-        return console.log(error) 
-    }
-    forecast(data.latitude,data.longitude,(error,forecastData)=>{
+if(!address){
+    console.log('Please provide address')
+}else{
+    geocode(address,(error,{latitude,longitude,location})=>{
         if(error){
-            return console.log(error)
+            return console.log(error) 
         }
-        console.log(data.location)
-        console.log(forecastData)
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return console.log(error)
+            }
+            console.log(location)
+            console.log(forecastData)
+        })
     })
-})
+}
+
+
 
